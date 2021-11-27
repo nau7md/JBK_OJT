62d90eeb42af9518e5d889b39842f18ef4e4803f
acer <acer@DESKTOP-QR0CVVP>
2021-11-27 20:19:26 +0530

    Changes are done.....

--- FirstSpringBoot/src/main/java/com/javabykiran/FirstSpringBoot/FirstSpringBootApplication.java
+++ FirstSpringBoot/src/main/java/com/javabykiran/FirstSpringBoot/FirstSpringBootApplication.java
@@ -12,7 +12,7 @@ public class FirstSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringBootApplication.class, args);
 		
-		System.out.println("Hello");
+		
 	}
 
 }
