ade870f42299ccf48380e5230be594fc1cfe19f5
Ansari <nau7md@outlook.com>
2022-06-24 18:37:40 +0530

    code changes are done. Need to test it.

--- FirstSpringBoot/src/main/java/com/javabykiran/FirstSpringBoot/FirstSpringBootApplication.java
+++ FirstSpringBoot/src/main/java/com/javabykiran/FirstSpringBoot/FirstSpringBootApplication.java
@@ -3,7 +3,6 @@ package com.javabykiran.FirstSpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 @ComponentScan(basePackages={"com.javabykiran"})
 @SpringBootApplication
@@ -12,7 +11,7 @@ public class FirstSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringBootApplication.class, args);
 		
-		System.out.println("Hello");
+		System.out.println("Application running successfully");
 	}
 
 }
--- FirstSpringBoot/src/main/java/com/javabykiran/controller/AdminController.java
+++ FirstSpringBoot/src/main/java/com/javabykiran/controller/AdminController.java
@@ -1,44 +1,17 @@
 package com.javabykiran.controller;
 
-import java.util.ArrayList;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.javabykiran.entity.Admin;
 
 @RestController
 public class AdminController {
 
-	@Value("${admin.id}")
-	int adminId;
-
-	@Value("${admin.name}")
-	String adminName;
-
-	@GetMapping("/getAllAdmin")
+	@GetMapping("/msg")
 	public String getAll() {
 
-		System.out.println("Admin id is:" + adminId + " " + "and Admin name is:" + adminName);
-
-		return "Get all Admin.......";
+		return "Hi, This is the first srping boot project";
 
 	}
 
-	@RequestMapping("/getId")
-	public int getId() {
-		return 103;
-	}
-
-	@GetMapping("/getAdmin")
-	public ArrayList<Admin> getAllAdmin() {
-		ArrayList<Admin> list = new ArrayList<Admin>();
-		list.add(new Admin(101, "Rishi", "Sales"));
-		list.add(new Admin(102, "Amit", "Admin"));
-		list.add(new Admin(103, "Nidhi", "Sales"));
-		return list;
-	}
 
 }
